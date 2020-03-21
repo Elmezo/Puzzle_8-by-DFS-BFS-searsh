ac857355cc2bf0b3f10118ad291d7db5989142da
Elmezo <36506058+Elmezo@users.noreply.github.com>
2020-03-21 14:48:43 +0200

    Update SearchTree.java

--- project_ai/SearchTree.java
+++ project_ai/SearchTree.java
@@ -1,7 +1,9 @@
 package project_ai;
+
 import java.util.*;
 
 public class SearchTree {
+
     private Node root;
     private String goalSate;
 
@@ -27,9 +29,9 @@ public class SearchTree {
     }
 
     public void breadthFirstSearch() {
-    
+
         Set<String> stateSets = new HashSet<>();
-        int totalCost = 0;
+
         int time = 0;
         Node node = new Node(root.getState());
         Queue<Node> queue = new LinkedList<>();
@@ -38,8 +40,9 @@ public class SearchTree {
             stateSets.add(currentNode.getState());
             List<String> nodeSuccessors = NodeUtil.getSuccessors(currentNode.getState());
             for (String n : nodeSuccessors) {
-                if (stateSets.contains(n))
+                if (stateSets.contains(n)) {
                     continue;
+                }
                 stateSets.add(n);
                 Node child = new Node(n);
                 currentNode.addChild(child);
@@ -55,24 +58,24 @@ public class SearchTree {
 
     }
 
-
     public void depthFirstSearch() {
-  
+
         Set<String> stateSets = new HashSet<>();
-        int totalCost = 0;
+
         int time = 0;
         Node node = new Node(root.getState());
-      
+
         MyQueue<Node> mainQueue = new MyQueue<>();
- 
+
         MyQueue<Node> successorsQueue = new MyQueue<>();
         Node currentNode = node;
         while (!currentNode.getState().equals(goalSate)) {
             stateSets.add(currentNode.getState());
             List<String> nodeSuccessors = NodeUtil.getSuccessors(currentNode.getState());
             for (String n : nodeSuccessors) {
-                if (stateSets.contains(n))
+                if (stateSets.contains(n)) {
                     continue;
+                }
                 stateSets.add(n);
                 Node child = new Node(n);
                 currentNode.addChild(child);
@@ -80,9 +83,9 @@ public class SearchTree {
                 successorsQueue.enqueue(child);
 
             }
-     
+
             mainQueue.addQueue(successorsQueue);
-       
+
             successorsQueue.clear();
             currentNode = mainQueue.dequeue();
             time += 1;
@@ -91,77 +94,5 @@ public class SearchTree {
         NodeUtil.printSolution(currentNode, stateSets, root, time);
 
     }
-
-
-  
-    public void uniformCostSearch() {
-    
-        Set<String> stateSets = new HashSet<>();
-        int totalCost = 0;
-        int time = 0;
-        Node node = new Node(root.getState());
-        node.setCost(0);
-
-        NodePriorityComparator nodePriorityComparator = new NodePriorityComparator();
-
-        PriorityQueue<Node> nodePriorityQueue = new PriorityQueue<>(10, nodePriorityComparator);
-        Node currentNode = node;
-        while (!currentNode.getState().equals(goalSate)) {
-            stateSets.add(currentNode.getState());
-            List<String> nodeSuccessors = NodeUtil.getSuccessors(currentNode.getState());
-            for (String n : nodeSuccessors) {
-                if (stateSets.contains(n))
-                    continue;
-                stateSets.add(n);
-                Node child = new Node(n);
-                currentNode.addChild(child);
-                child.setParent(currentNode);
-                child.setTotalCost(currentNode.getTotalCost() + Character.getNumericValue(child.getState().charAt(child.getParent().getState().indexOf('0'))), 0);
-                nodePriorityQueue.add(child);
-
-            }
-            currentNode = nodePriorityQueue.poll();
-            time += 1;
-        }
-        NodeUtil.printSolution(currentNode, stateSets, root, time);
-
-    }
-
-    public void bestFirstSearch() {
-   
-        Set<String> stateSets = new HashSet<>();
-        int totalCost = 0;
-        int time = 0;
-        Node node = new Node(root.getState());
-        node.setCost(0);
-
-      
-        NodePriorityComparator nodePriorityComparator = new NodePriorityComparator();
-
-        PriorityQueue<Node> nodePriorityQueue = new PriorityQueue<>(10, nodePriorityComparator);
-        Node currentNode = node;
-        while (!currentNode.getState().equals(goalSate)) {
-            stateSets.add(currentNode.getState());
-            List<String> nodeSuccessors = NodeUtil.getSuccessors(currentNode.getState());
-            for (String n : nodeSuccessors) {
-                if (stateSets.contains(n))
-                    continue;
-                stateSets.add(n);
-                Node child = new Node(n);
-                currentNode.addChild(child);
-                child.setParent(currentNode);
-
-                
-                nodePriorityQueue.add(child);
-
-            }
-            currentNode = nodePriorityQueue.poll();
-            time += 1;
-        }
-     
-        NodeUtil.printSolution(currentNode, stateSets, root, time);
-
-    }
-
 
 }
